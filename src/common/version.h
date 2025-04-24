ddf172043d98bb33c1fc8512e60b920eee230f8d
xXJSONDeruloXx <danielhimebauch@gmail.com>
2025-04-23 21:25:42 -0400

    fix: add version.h

---
+++ src/common/version.h
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define PROJECT_VERSION "0.8.1"
+#define PROJECT_VERSION_MAJOR 0
+#define PROJECT_VERSION_MINOR 8
+#define PROJECT_VERSION_PATCH 1
